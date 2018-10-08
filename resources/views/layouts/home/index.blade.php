038231349130e5ad107a0feccf382cd27f99105b
lidi <lidi0903@163.com>
2018-10-08 10:39:40 +0800

    修改资源

--- resources/views/layouts/home/index.blade.php
+++ resources/views/layouts/home/index.blade.php
@@ -12,7 +12,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link href="https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link rel="stylesheet" href="/css/home/style.css?v=1.0">
         @yield('css')
@@ -51,7 +51,7 @@
                     @if(session('home.name'))
                         <div>
                             <img style="height: 40px;" class="avatar-topnav" alt="{{ session('home.name') }}" src="{{ session('home.avatar') }}">
-                            <span>{{ session('home.name') }}</span> | 
+                            <span>{{ session('home.name') }}</span> |
                             <span><a href="{{ route('oauth.logout') }}">退出</a></span>
                         </div>
 
@@ -95,7 +95,7 @@
                         </div>
 
                         <ul class="list-group list-group-flush f-14">
-                            @foreach($topArticle as $v)                            
+                            @foreach($topArticle as $v)
                                 <li class="list-group-item d-flex justify-content-between align-items-center pr-2 py-2">
                                     <a class="category-item" href="{{ url('article', [$v->id]) }}" title="{{ $v->title }}">{{ $v->title }}</a>
                                 </li>
@@ -146,8 +146,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                        <span>Copyright © 2018 <a href="http://lidicode.com/">李迪博客</a></span> | 
-                        <span><a href="http://www.miibeian.gov.cn/" target="_blank">@if(!empty($config['WEB_ICP_NUMBER'])) {{ $config['WEB_ICP_NUMBER'] }} @endif</a></span> | 
+                        <span>Copyright © 2018 <a href="http://lidicode.com/">李迪博客</a></span> |
+                        <span><a href="http://www.miibeian.gov.cn/" target="_blank">@if(!empty($config['WEB_ICP_NUMBER'])) {{ $config['WEB_ICP_NUMBER'] }} @endif</a></span> |
                         <span><img src="/images/home/beian.png"><a href="http://www.beian.gov.cn/portal/registerSystemInfo" target="_blank">京公网安备 11011202001314号</a></span>
                     </div>
                     <div class="col-sm-12">
@@ -182,9 +182,9 @@
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdn.bootcss.com/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         @yield('js')
         <script type="text/javascript" src="/js/home/lidicode.js?v=1.0"></script>
         <script>
